d2c66dd7089fd92b5a4bbb63bef2405f7ff41ca1
m.ezzeddine <mazen.ezzeddine@gmail.com>
2023-12-17 13:37:33 +0100

    bin pack with prometheus scraping

---
+++ src/main/java/Main.java
@@ -0,0 +1,59 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    private static final Logger log = LogManager.getLogger(Main.class);
+
+    static BinPack3pp bp;
+
+
+   // static BinPack3Strimzi bp;
+
+
+    //static BinPack3pp10 bp;
+
+
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        bp = new BinPack3pp();
+
+        //bp = new BinPack3Strimzi();
+        log.info("Warming for 10 seconds.");
+        Thread.sleep(10*1000);
+        while (true) {
+            log.info("Querying Prometheus");
+            Main.QueryingPrometheus();
+            log.info("Sleeping for 1 seconds");
+            log.info("========================================");
+            Thread.sleep(5000);
+        }
+    }
+
+
+    static void QueryingPrometheus() throws ExecutionException, InterruptedException {
+        ArrivalRates.arrivalRateTopic1();
+
+        //ArrivalRatesStrimzi.arrivalRateTopic1();
+
+        if (ArrivalRates.processingRate != 0) {
+            scaleLogic();
+        }
+    }
+
+
+    private static void scaleLogic() throws InterruptedException, ExecutionException {
+        if  (Duration.between(bp.LastUpScaleDecision, Instant.now()).getSeconds() >10){
+           // ArrivalRatesStrimzi.arrivalRateTopic1();
+
+            bp.scaleAsPerBinPack();
+        } else {
+            log.info("No scale group 1 cooldown");
+        }
+    }
+
+}
